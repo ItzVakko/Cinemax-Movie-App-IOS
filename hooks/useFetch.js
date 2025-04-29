5d73a0c52d7d40d5cf4b015dfed585217cc47903
ItzVakko <itzvakkoofficial@gmail.com>
2025-04-29 16:12:17 +0400

    added useFetch hook

---
+++ hooks/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (fetchFunction, autoFetch = true) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setLoading(true);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, reset, fetchData };
+};
+
+export default useFetch;
