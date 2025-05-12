d515821aff0b9ec7365a9e02396c4c430ea288d5
ItzVakko <itzvakkoofficial@gmail.com>
2025-05-12 21:34:04 +0400

    made loading, error, empty components for wishlist

--- app/tabs/Wishlist.js
+++ app/tabs/Wishlist.js
@@ -1,4 +1,12 @@
-import { View, Text, Platform, Pressable, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  Platform,
+  Pressable,
+  FlatList,
+  Image,
+  ActivityIndicator,
+} from "react-native";
 import BackArrow from "../../assets/icons/BackArrow";
 import { useNavigation } from "@react-navigation/native";
 import WishlistCard from "../components/WishlistCard/WishlistCard";
@@ -6,13 +14,14 @@ import useFetch from "../../hooks/useFetch";
 import { useEffect, useState } from "react";
 import useAuthStore from "../../store/authStore";
 import { fetchWishlistMovies } from "../../services/movieApi";
+import EmptyBoxImage from "../../assets/images/empty-box.png";
 
 const Wishlist = () => {
   const navigation = useNavigation();
 
   const user = useAuthStore((state) => state.user);
 
-  const { data, fetchData } = useFetch({
+  const { data, loading, error, fetchData } = useFetch({
     fetchFunction: () => fetchWishlistMovies(user.wishlist),
   });
 
@@ -25,7 +34,7 @@ const Wishlist = () => {
       className="flex-1 bg-primary-dark px-4"
       style={{ paddingTop: Platform.OS === "ios" ? 60 : 0 }}
     >
-      <View className="px-4 flex-row justify-between items-center">
+      <View className="flex-row justify-between items-center">
         <Pressable
           className="bg-primary-soft w-8 h-8 rounded-xl justify-center items-center"
           onPress={() => navigation.navigate("Home")}
@@ -43,7 +52,25 @@ const Wishlist = () => {
         data={data}
         keyExtractor={(item) => String(item.id)}
         renderItem={({ item }) => <WishlistCard {...item} />}
+        showsVerticalScrollIndicator={false}
         ItemSeparatorComponent={() => <View className="h-4" />}
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="items-center gap-2 mt-[50%]">
+              <Image source={EmptyBoxImage} />
+              <Text className="text-text-whiteGrey text-base text-center font-semibold tracking-[0.12px] leading-[160%] max-w-[160px]">
+                There is no movie yet!
+              </Text>
+              <Text className="text-text-grey text-xs text-center max-w-[200px]">
+                Find your favourite movie, press heart and add to Wishlist
+              </Text>
+            </View>
+          ) : null
+        }
+        ListHeaderComponent={
+          (loading ? <ActivityIndicator className="my-8" /> : null) ||
+          (error && <Text>Error: {error}</Text>)
+        }
       />
     </View>
   );
